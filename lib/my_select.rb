10421c984501eb7687ada017234bd3eba4b6dafe
angelica winkler <caw400@nyu.edu>
2018-07-24 15:41:05 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  newArray = []
+
+    i = 0
+    while i <array.length
+      if yield(array[i]) == true
+        newArray << yield(array[i])
+        i += 1
+      end
+    end
+  else
+    puts "No block given!"
+  end
+  
+
+  newArray
 end
